b4e76f49c11d48128015a97c88a1ce1867eba27d
Viggo Johnsson <viggo.johnsson1@gmail.com>
2024-01-22 22:37:36 +0100

    titlescreeeeeen

--- text.rb
+++ text.rb
@@ -2,17 +2,14 @@ require 'ruby2d'
 set width: 800
 set height: 600
 
-@players = [
-
-    Sprite.new(
-      y: 300,
-      'player_1.png',
-      clip_width: 32,
-      width: 50 * 2,
-      height: 50 * 2
-
-    )
-]
-
-
+Text.new(
+  'Player 1',
+size: 60,
+y: 100
+)
+Text.new(
+  'Player 2',
+size: 60,
+y: 200
+)
 show
--- titlescreen.rb
+++ titlescreen.rb
@@ -20,6 +20,19 @@ class Star
   end
 end
 
+class Player
+    WIDTH = 223
+    HEIGHT = 63
+  def initialize(image, x, y)
+    Sprite.new(
+        image,
+        width: WIDTH,
+        height: HEIGHT,
+        x: x,
+        y: y
+      )
+  end
+end
 class PlayerSelectScreen
   def initialize
     @stars = Array.new(100).map { Star.new }
@@ -30,8 +43,10 @@ class PlayerSelectScreen
     gamemode_text = Text.new('Gamemode Select', size: 50, y: 220)
     gamemode_text.x = (Window.width - gamemode_text.width) /2
 
-    player_text = Text.new('Player 1', size: 50 , y: 375)
-    player_text.x = (Window.width - player_text.width) / 5
+    @players = [
+       Player.new('better_player1_wb.png', Window.width * (1/4.0) - Player::WIDTH / 2, 375), #float på ena för annars tror ruby det är noll
+       Player.new('player_2.png', Window.width * (3/4.0) - Player::WIDTH / 2, 375)
+    ]
   end
 
 
