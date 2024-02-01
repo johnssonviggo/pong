4cfec610a7c2f8cb8faa09469a20f271a24527eb
johnssonviggo <viggo.johnsson1@gmail.com>
2024-02-01 16:02:50 +0100

    stars

--- pong.rb
+++ pong.rb
@@ -67,7 +67,7 @@
     end
 
     def draw
-      @shape = Square.new(x: @x, y: @y, size: HEIGHT, color: 'white')
+      @shape.add
     end
 
     def bounce
@@ -85,78 +85,16 @@
     end
   end
 
-  class Star
-
-    def initialize
-      @y_velocity = rand(1..4)
-      @shape = Square.new(
-        x: rand(Window.width),
-        y: rand(Window.height),
-        size: rand(3..5),
-        color: 'random')
-    end
-
-    def move
-      @shape.y = (@shape.y + @y_velocity) % Window.height
-    end
-  end
-
-  class Player
-      WIDTH = 223
-      HEIGHT = 63
-    def initialize(image, x, y)
-      Sprite.new(
-        image,
-        width: WIDTH,
-        height: HEIGHT,
-        x: x,
-        y: y
-      )
-    end
-  end
-
-  class PlayerSelectScreen
-    def initialize
-      @stars = Array.new(100).map { Star.new }
-
-      title_text = Text.new('Pong', size: 100, y: 100)
-      title_text.x = (Window.width - title_text.width) /2
-
-      gamemode_text = Text.new('Gamemode Select', size: 50, y: 220)
-      gamemode_text.x = (Window.width - gamemode_text.width) /2
-
-      @players = [
-        Player.new('better_player1_wb.png', Window.width * (1/4.0) - Player::WIDTH / 2, 375), #float på ena för annars tror ruby det är noll
-        Player.new('player_2.png', Window.width * (3/4.0) - Player::WIDTH / 2, 375)
-      ]
-    end
-
-    def update_stars
-      if Window.frames % 2 == 0
-        @stars.each { |star| star.move}
-      end
-    end
-  end
-
   player_1 = Racket.new(:left, 5)
   player_2 = Racket.new(:right, 5)
   ball = Ball.new (4)
-  star = Star.new
-
-  player_select_screen = PlayerSelectScreen.new
-
+  ball.add
 
   update do
-
-    star.move
-    player_select_screen.update_stars
 
     if player_1.hit_ball?(ball) || player_2.hit_ball?(ball)
       ball.bounce
     end
-
-    star.move
-    star.draw
 
     player_1.move_racket
     player_1.draw
@@ -165,7 +103,7 @@
     player_2.draw
 
     ball.move_ball
-    ball.draw
+
 
   end
 
---
+++ stars.rb
@@ -0,0 +1,122 @@
+require 'ruby2d'
+
+set width: 800
+set height: 600
+
+class Star
+  def initialize
+    @shape = Square.new(
+      x: rand(Window.width),
+      y: rand(Window.height),
+      size: rand(3..5),
+      color: 'random')
+    @y_velocity = rand(1..3)
+  end
+
+  def move
+    @shape.y = (@shape.y + @y_velocity) % Window.height
+  end
+
+  def draw
+    @shape.add
+  end
+end
+
+class Player
+  HEIGHT = 150
+  attr_writer :direction
+  def initialize(side, movement_speed)
+    @direction = nil
+    @movement_speed = movement_speed
+    @y = 200
+      if side == :left
+        @x = 60
+      else
+        @x = 720
+      end
+      @paddle = Rectangle.new(x: @x , y: @y,
+      width: 25 , height: HEIGHT,
+      color: 'white')
+      @x_speed = 0
+      @y_speed = 0
+  end
+
+  def draw
+    @shape.add
+  end
+
+  def move
+    if @direction == :up
+      @y = [@y - @movement_speed, 0].max
+    elsif @direction == :down
+      @y = [@y + @movement_speed, max_y].min
+    end
+  end
+
+  private
+
+  def max_y
+    Window.height - HEIGHT
+  end
+end
+
+# class Player
+#   def initialize
+#     @shape = Square.new(
+#       x: Window.width / 2,
+#       y: Window.height - 50,
+#       size: 20,
+#       color: 'white')
+#     @x_velocity = 1
+#   end
+
+#   def move_left
+#     @shape.x -= @x_velocity if @shape.x - @x_velocity >= 0
+#   end
+
+#   def move_right
+#     @shape.x += @x_velocity if @shape.x + @x_velocity + @shape.size <= Window.width
+#   end
+
+#   def draw
+#     @shape.add
+#   end
+# end
+
+class StarManager
+  def initialize
+    @stars = Array.new(100).map { Star.new }
+  end
+
+  def draw
+    @stars.each(&:draw)
+  end
+
+  def update
+    @stars.each(&:move)
+  end
+end
+
+star_manager = StarManager.new
+player = Player.new(:left, 5)
+
+
+update do
+
+  star_manager.update
+  star_manager.draw
+
+  player.move
+end
+
+on :key_down do |event|
+  if event.key == 'w'
+    @direction = :up
+  elsif event.key == 's'
+    @direction = :down
+  end
+end
+
+
+
+show
